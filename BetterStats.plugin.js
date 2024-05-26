c206eb55991d9f9b1b75a418ec26b66afcebebd8
nkplka <nkplkaa@gmail.com>
2024-05-27 02:10:40 +0300

    Update BetterStats.plugin.js

--- BetterStats.plugin.js
+++ BetterStats.plugin.js
@@ -1,6 +1,6 @@
 /**
  * @name BetterStats
- * @version 0.1.2
+ * @version 0.0.1
  * @description Tracks various user statistics in Discord.
  * @author z3phyr
  */
@@ -17,7 +17,7 @@ const config = {
                 name: "z3phyr"
             }
         ],
-        version: "0.1.2",
+        version: "0.0.1",
         description: "Tracks various user statistics in Discord."
     },
     defaultConfig: []
